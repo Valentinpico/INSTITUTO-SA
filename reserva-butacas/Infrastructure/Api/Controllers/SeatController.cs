f0818e7d04e3fcf211da43abc34dafbc23d0c2ce
Valentín Pico <86754349+Valentinpico@users.noreply.github.com>
2025-03-07 20:05:23 -0500

    Agregando archivos no rastreados

---
+++ reserva-butacas/Infrastructure/Api/Controllers/SeatController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace reserva_butacas.Infrastructure.Api.Controllers
+{
+    [Route("[controller]")]
+    public class SeatController : Controller
+    {
+        private readonly ILogger<SeatController> _logger;
+
+        public SeatController(ILogger<SeatController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
