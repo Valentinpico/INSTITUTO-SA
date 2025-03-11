4f852eba3d9094856415aff1c5dfc5597fc0cab7
Valentín Pico <86754349+Valentinpico@users.noreply.github.com>
2025-03-10 21:36:25 -0500

    sdf

--- reserva-butacas/obj/Debug/net8.0/reserva-butacas.AssemblyInfo.cs
+++ reserva-butacas/obj/Debug/net8.0/reserva-butacas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("reserva-butacas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd7a097d13e9cc743b7333f1738d5bc277e89c27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a7cb1c4d0e419eca5bdb9fe668fcd7dc99632a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("reserva-butacas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("reserva-butacas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
