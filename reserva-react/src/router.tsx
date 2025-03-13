6972201f806abca037bec68902e0c953ac3ad0d7
Valentín Pico <86754349+Valentinpico@users.noreply.github.com>
2025-03-12 19:58:47 -0500

    ultimo bokking

--- reserva-react/src/router.tsx
+++ reserva-react/src/router.tsx
@@ -1,12 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Home } from "./Home";
 import { Layout } from "./layouts/Layout";
-import { BillboardPage } from "./modules/Billboard/BillboardPage";
-import { BookingPage } from "./modules/Booking/BookingPage";
+import { BillboardPage } from "./modules/Billboard/AdminCartelera";
+import { BookingPage } from "./modules/Booking/ReservationList";
 import { CustomerPage } from "./modules/Customer/CustomerPage";
 import { MoviePage } from "./modules/Movie/MoviePage";
 import { RoomPage } from "./modules/Room/RoomPage";
-import { SeatPage } from "./modules/Seat/SeatPage";
+import { SeatPage } from "./modules/Seat/AdminButacas";
 
 export const Router = createBrowserRouter([
   {
